a6ea7a93bf619c7f3cc220f28c7b9ea38ad53f6d
Brittany Morris <brittanymorrisdesign@gmail.com>
2020-04-20 18:31:00 -0400

    added components, pages, and app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Nav from "./components/Nav/nav";
+import Search from "./pages/Search/search";
+import Saved from "./pages/Saved/saved";
+import NoMatch from "./pages/NoMatch/nomatch";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <div id="main-content">
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Search} />
+            <Route exact path="/saved" component={Saved} />
+            <Route path="*" component={NoMatch} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
