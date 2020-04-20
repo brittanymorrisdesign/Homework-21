d053a4eda99a73901cdcbd6c44a56fde8c0d2994
Brittany Morris <brittanymorrisdesign@gmail.com>
2020-04-20 18:09:35 -0400

    added server.js

---
+++ models/book.js
@@ -0,0 +1,18 @@
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  _id: { type: String },
+  title: { type: String },
+  authors: { type: Array },
+  description: String,
+  image: String,
+  link: String,
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
+
+
